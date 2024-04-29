07787a71f5a5d3ffe7da8a3a7f3ba224d8d16479
liliia_vintiuk <rudapanda2016@gmail.com>
2024-04-29 20:22:34 +0300

    update

--- routes/main.js
+++ routes/main.js
@@ -7,23 +7,63 @@ app.get('/', (req, res) => {
   
 // all list
 app.get('/api/items', (req, res) => {
-  return res.send(JSON.stringify());
+  return res.send(JSON.stringify(items));
 });
-app.get('/api/items/:id', (req, res) => {
-  return res.send(JSON.stringify());
-});
-  
 
-  app.post('/api/items', (req, res) => {
+//viens id
+app.get('/api/items/:id', (req, res) => {
+  const item = +req.params.id
+  if(item){
+  return res.send(JSON.stringify(
+  items.filter(index => index.id = item)));
+  }else if(!item){
+  console.log('404');
+  return res.send('Item not found')
+}
+});
+
+//sukurti id
+app.post('/api/items/create', (req, res) => {
+  const { id, name, price } = req.body;
+
+  items.push(id, name, price )
   
+  res.status(201).json(newItem);
   });
   
 
   app.put('/api/items/:id', (req, res) => {
-
-  });
+    const itemId = req.params.id;
+    const { name, price } = req.body;
   
+    const item = items.find(item => item.id === itemId);
+  
+    
+    if (!item) {
+      return res.status(404).json({ error: 'Товар не знайдено' });
+    }
+  
+    if (name) item.name = name;
+    if (price) item.price = price;
+  
+    res.json(item);
+  });
 
   app.delete('/api/items/:id', (req, res) => {
-  
+    const itemId = req.params.id;
+  items.filter(index => index.id !== item)
   });
+
+  app.get('*', (req, res) => {
+    console.log('404');
+    return res.send('404 - content not found');
+});
+
+app.use((req, res, next) => {
+    return res.status(404).send("Sorry can't find that!");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
